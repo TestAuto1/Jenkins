b01c03bc4e6e3844b6d1390857f7f03b72cfb16a
GamilM <GamilM@192.168.56.1>
2018-02-01 08:11:35 +0200

    1st commit

---
+++ SeleniumwithJenkins/src/test/java/Tests/TC_001.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test				
+	public void Dummy_test_to_pass() 
+	{									 
+	Assert.assertEquals(1,1); 		
+	}	
+	@Test				
+	 public void Dummy_test_to_fail()
+	 {								 
+	Assert.assertEquals(1,2);
+	
+	}
+	
+	
+
+}
